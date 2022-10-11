98322adf83f217f0ff3d0df2c3be3de19625e4ac
甜糖 <2956548350@qq.com>
2022-10-11 18:02:26 +0800

    gettest

---
+++ WebApplication1/WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Configuration;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : ApiController
+    {
+        public IHttpActionResult GetHello ()
+        {
+            return Json("hello world!");
+        }
+        public IHttpActionResult GetStudentList()
+        {
+            string strConn = ConfigurationManager.ConnectionStrings["StudentDB"].ConnectionString;
+            var sql = "select * from data";
+            using (var conn = new SqlConnection(strConn))
+            {
+                var result = conn.Query<dynamic>(sql);
+                return Json(result.ToList());
+            }
+        }
+    }
+}
